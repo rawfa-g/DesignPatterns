7f190f33f77816d5ac846b3936610d049bb27c9c
Rafa Gasull <rafa.gasull.m@gmail.com>
2022-09-15 22:33:12 +0200

    Curso TypeScript
    Finalizacion practica sesión 4

--- app.js
+++ app.js
@@ -1,21 +1,60 @@
 "use strict";
 //Cadena de valores que puede tomar un codigo de color
-const hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+const hexValues = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+];
 //constante btn que cogemos del id btn del html
 const btn = document.getElementById("btn");
 //lo mismo con el color. ¿?QuerySelector?¿?
 const color = document.querySelector(".color");
 //Above hace que cuando cliquemos se imprima en consola el body del documento html.
+// btn.addEventListener("click", function () {
+//   //Estamos realizando una cadena que recorre 6 veces el valor que es la cadena #000000
+//   let hexColor: string = "#";
+//   for (let i = 0; i < 6; i++) {
+//     console.log(getRandomNumber());
+//     hexColor += hexValues[getRandomNumber()];
+//   }
+//   color.textContent = hexColor;
+//   document.body.style.backgroundColor = hexColor;
+// });
+// btn.addEventListener("click", function () {
+//   generateColor((x) => {
+//     color.textContent = x;
+//     document.body.style.backgroundColor = x;
+//   });
+// });
 btn.addEventListener("click", function () {
-    //Estamos realizando una cadena que recorre 6 veces el valor que es la cadena #000000   
+    generateColor(updateUI);
+});
+function updateUI(hexColor) {
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+}
+//uso de callbacks
+function generateColor(update) {
     let hexColor = "#";
     for (let i = 0; i < 6; i++) {
         console.log(getRandomNumber());
         hexColor += hexValues[getRandomNumber()];
     }
-    color.textContent = hexColor;
-    document.body.style.backgroundColor = hexColor;
-});
+    update(hexColor);
+}
 function getRandomNumber() {
     return Math.floor(Math.random() * hexValues.length);
 }
--- app.ts
+++ app.ts
@@ -1,5 +1,22 @@
 //Cadena de valores que puede tomar un codigo de color
-const hexValues:string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+const hexValues: string[] = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+];
 
 //constante btn que cogemos del id btn del html
 const btn = document.getElementById("btn")!;
@@ -8,19 +25,44 @@ const btn = document.getElementById("btn")!;
 const color = document.querySelector(".color")!;
 
 //Above hace que cuando cliquemos se imprima en consola el body del documento html.
-btn.addEventListener("click", function(){
-    
-    //Estamos realizando una cadena que recorre 6 veces el valor que es la cadena #000000   
-    let hexColor:string = "#";
-    for(let i=0; i < 6; i++){
-        console.log(getRandomNumber())
-        hexColor += hexValues[getRandomNumber()];
-    }
+// btn.addEventListener("click", function () {
+//   //Estamos realizando una cadena que recorre 6 veces el valor que es la cadena #000000
+//   let hexColor: string = "#";
+//   for (let i = 0; i < 6; i++) {
+//     console.log(getRandomNumber());
+//     hexColor += hexValues[getRandomNumber()];
+//   }
 
-    color.textContent = hexColor;
-    document.body.style.backgroundColor = hexColor;
+//   color.textContent = hexColor;
+//   document.body.style.backgroundColor = hexColor;
+// });
+
+// btn.addEventListener("click", function () {
+//   generateColor((x) => {
+//     color.textContent = x;
+//     document.body.style.backgroundColor = x;
+//   });
+// });
+
+btn.addEventListener("click", function () {
+  generateColor(updateUI);
 });
 
-function getRandomNumber(){
-    return Math.floor(Math.random() * hexValues.length);
+function updateUI(hexColor: string) {
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+}
+
+//uso de callbacks
+function generateColor(update: (color: string) => void) {
+  let hexColor: string = "#";
+  for (let i = 0; i < 6; i++) {
+    console.log(getRandomNumber());
+    hexColor += hexValues[getRandomNumber()];
+  }
+  update(hexColor);
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hexValues.length);
 }
